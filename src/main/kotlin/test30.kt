dc48e6e58ff1327d0bf15bd5171f4194124ef9ec
wowhs <macgigamac@gmail.com>
2018-04-13 13:12:23 +0900

    commit2 from mac

--- src/main/kotlin/test30.kt
+++ src/main/kotlin/test30.kt
@@ -1,3 +1,6 @@
 fun main(args: Array<String>) {
     listOf(1,2,3,4,5).filter { it > 2 }.map { it * 2 }.forEach { println(it) }
+
+    listOf(1,2,3,4,5).filter { it > 2 }.map { it * 2 }.forEach { println(it) }
+    listOf(1,2,3,4,5).filter { it > 2 }.map { it * 2 }.forEach { println(it) }
 }
